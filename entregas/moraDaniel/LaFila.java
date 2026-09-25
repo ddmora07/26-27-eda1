cfa2da97eafadc728b66d6bd619608e77f65d0be
Danielmora <daniel.mora@alumnos.uneatlantico.es>
2026-09-25 10:51:50 +0200

    La fila reformado

--- entregas/moraDaniel/LaFila.java
+++ entregas/moraDaniel/LaFila.java
@@ -4,7 +4,7 @@ public class LaFila {
         System.out.println(" Reto base: 4 horas, sin reglas extendidas ");
         Simulador simuladorDelRetoBase = new Simulador();
         simuladorDelRetoBase.simular(240, false);
-
+ 
         System.out.println();
         System.out.println(" Reto completo: 2 horas, reglas extendidas desde el minuto 20 ");
         Simulador simuladorDelRetoCompleto = new Simulador();
