27dbc5d678f1142f8a7a1e4afec9994e1daa9a8d
Danielmora <daniel.mora@alumnos.uneatlantico.es>
2026-09-25 10:53:55 +0200

    Persona reformado

--- entregas/moraDaniel/Persona.java
+++ entregas/moraDaniel/Persona.java
@@ -1,34 +1,35 @@
+
 public class Persona {
- 
+
     public static final int SIN_PRIORIDAD = 0;
     public static final int CON_PRIORIDAD_PREFERENTE = 1;
- 
+
     private final int identificador;
     private final int nivelDePrioridad;
     private final int minutoDeLlegada;
- 
+
     public Persona(int identificador, int nivelDePrioridad, int minutoDeLlegada) {
         this.identificador = identificador;
         this.nivelDePrioridad = nivelDePrioridad;
         this.minutoDeLlegada = minutoDeLlegada;
     }
- 
+
     public int getIdentificador() {
         return identificador;
     }
- 
+
     public int getNivelDePrioridad() {
         return nivelDePrioridad;
     }
- 
+
     public int getMinutoDeLlegada() {
         return minutoDeLlegada;
     }
- 
+
     public boolean tienePrioridadPreferente() {
         return nivelDePrioridad == CON_PRIORIDAD_PREFERENTE;
     }
- 
+
     public String toString() {
         if (tienePrioridadPreferente()) {
             return "Persona" + identificador + "(preferente)";
